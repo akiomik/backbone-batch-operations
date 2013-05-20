3fdd09178c12cace28256a238a903f9d53d14cb4
Akiomi Kamakura <akiomik@gmail.com>
2013-05-21 02:19:20 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,48 @@
+backbone-batch-operations
+--------------------------
+This is a backbone.js plugin provides batch operations.
+
+## Installation
+Add following line to your html.
+
+```html
+<script src="javascripts/backbone-batch-operations.js" type="text/javascript"></script>
+```
+
+## Sample
+
+```javascript
+var Document = Backbone.Model.extend({});
+var Documents = Backbone.BatchCollection.extend({
+    model: Document,
+    url: 'http://example.com'
+});
+var docs = new Documents({});
+// Insert
+var models = [
+    {'some': 'data'}
+];
+docs.add(models, {silent: true});
+docs.save();    // POST
+// Update
+docs.each(function(doc) {
+    var json = doc.toJSON();
+    json.some = 'data was modified';
+    doc.set(json, {silent: true});
+});
+docs.save(); // PUT
+// Delete
+var models = docs.models;
+docs.remove(models, {silent: true});
+docs.save();    // PUT
+```
+
+## Reference
+### collection.save([options]);
+
+### collection.isNew();
+
+### collection.isChanged();
+
+## License
+The MIT License. See `LICENSE` file.
---
+++ backbone-batch-operations.js
@@ -0,0 +1,83 @@
+(function() {
+    Backbone.BatchCollection = Backbone.Collection.extend({
+        isNew: function() {
+            var isNew = this.all(function(model) {
+                return !_.has(model, 'id');
+            });
+            return isNew;
+        },
+        isChanged: function() {
+            var isChanged = this.some(function(model) {
+                return model.hasChanged();
+            });
+            return isChanged;
+        },
+        save: function(options) {
+            // initialize options
+            if (_.isUndefined(options)) {
+                options = {};
+            }
+            _.defaults(options, {
+                url: this.url,
+                contentType: 'application/json',
+                silent: false,
+                data: this.toJSON()
+            });
+
+            // check collection status
+            if (this.isEmpty()) {
+                this.batchDelete(options);
+            }
+            else if (this.isNew()) {
+                this.batchAdd(options);
+            }
+            else {
+                this.batchUpdate(options);
+            }
+        },
+        batch: function(options) {
+            // create parameters
+            var ajaxOptions = {
+                type: options.type,
+                url: options.url,
+                data: JSON.stringify(options.data),
+                contentType: options.contentType,
+                context: this
+            };
+
+            // batch update models
+            $.ajax(ajaxOptions).done(function(res) {
+                if (!options.silent) {
+                    this.trigger('sync', this.models);
+                }
+            });
+
+            // fire request event
+            if (!options.silent) {
+                this.trigger('request', this.models);
+            }
+
+        },
+        batchDelete: function(options) {
+            if (_.isUndefined(options.type)) {
+                options.type = 'PUT';
+            }
+            this.batch(options);
+        },
+        batchUpdate: function(options) {
+            if (_.isUndefined(options.type)) {
+                options.type = 'PUT';
+            }
+            this.batch(options);
+        },
+        batchAdd: function(options) {
+            if (_.isUndefined(options.type)) {
+                options.type = 'POST';
+            }
+            this.batch(options);
+        }
+    });
+
+    _.extend(Backbone.BatchCollection.prototype, Backbone.Event);
+}).call(this);
+
